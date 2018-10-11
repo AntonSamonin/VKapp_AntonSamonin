0b8fc849a034d0576234e8ac55568447cd4b8bf8
Anton Samonin <antonsamonin93@gmail.com>
2018-10-11 20:05:02 +0400

    cell

--- VKapp_AntonSamonin/FriendsPhoto/FriendsPhotoController.swift
+++ VKapp_AntonSamonin/FriendsPhoto/FriendsPhotoController.swift
@@ -57,7 +57,7 @@ class FriendsPhotoController: UICollectionViewController {
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of items
-        return  1
+        return  2
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
